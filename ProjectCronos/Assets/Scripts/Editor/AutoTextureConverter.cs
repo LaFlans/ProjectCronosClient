c02e98f44b33d33706d5a571ec0f3bc0d6a52742
LaFlans <shouya726@gmail.com>
2022-06-30 06:23:34 +0900

    一旦コメントアウト

--- ProjectCronos/Assets/Scripts/Editor/AutoTextureConverter.cs
+++ ProjectCronos/Assets/Scripts/Editor/AutoTextureConverter.cs
@@ -5,19 +5,19 @@ namespace ProjectCronos
 {
     public class AutoTextureConverter : AssetPostprocessor
     {
-        /// <summary>
-        /// テクスチャがプロジェクトにimportされる直前に呼び出される
-        /// </summary>
-        void OnPreprocessTexture()
-        {
-            TextureImporter importer = assetImporter as TextureImporter;
+        ///// <summary>
+        ///// テクスチャがプロジェクトにimportされる直前に呼び出される
+        ///// </summary>
+        //void OnPreprocessTexture()
+        //{
+        //    TextureImporter importer = assetImporter as TextureImporter;
 
-            // FIXME: プロジェクト設定とかに文字列の設定を持っていく
-            if (importer.name.Contains("_Normal"))
-            {
-                // テクスチャ名に指定の文字があった場合、テクスチャタイプを法線マップにする
-                importer.textureType = TextureImporterType.NormalMap;
-            }
-        }
+        //    // FIXME: プロジェクト設定とかに文字列の設定を持っていく
+        //    if (importer.name.Contains("_Normal"))
+        //    {
+        //        // テクスチャ名に指定の文字があった場合、テクスチャタイプを法線マップにする
+        //        importer.textureType = TextureImporterType.NormalMap;
+        //    }
+        //}
     }
 }
