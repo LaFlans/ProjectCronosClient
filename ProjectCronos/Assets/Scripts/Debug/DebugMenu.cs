3a4baaf369f5a891fd65ef972963f5691e91c474
LaFlans <shouya726@gmail.com>
2022-12-13 23:01:11 +0900

    デバックメニューを追加

---
+++ ProjectCronos/Assets/Scripts/Debug/DebugMenu.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+namespace ProjectCronos
+{
+    public class DebugMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// デバックメニュー更新頻度
+        /// </summary>
+        const float updateSpan = 1f;
+        bool isAutoUpdateView;
+        float currentTime;
+
+        [SerializeField]
+        CanvasGroup parentCanvasGroup;
+
+        [SerializeField]
+        TextMeshProUGUI coinText;
+        [SerializeField]
+        TextMeshProUGUI playTimeText;
+
+        /// <summary>
+        /// プレイヤーステータス参照
+        /// </summary>
+        PlayerStatus playerStatus;
+
+        /// <summary>
+        /// デバックメニューを表示するか
+        /// </summary>
+        bool isShow;
+
+        void Start()
+        {
+            isShow = true;
+            isAutoUpdateView = false;
+            currentTime = 0f;
+            parentCanvasGroup.alpha = isShow ? 1 : 0;
+        }
+
+        public void Initialize()
+        {
+            // プレイヤー取得、イベント登録
+            playerStatus = GameObject.FindWithTag("Player").GetComponent<PlayerStatus>();
+            playerStatus.ResisterStatusUpdateEvent(UpdateView);
+
+            // ビュー更新
+            UpdateView();
+
+            // 自動ビュー更新をするようにする
+            isAutoUpdateView = true;
+
+            // 入力設定
+            InputManager.Instance.inputActions.Debug.ShowDebugMenu.performed += OnShowDebugMenu;
+        }
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+            if (isAutoUpdateView)
+            {
+                currentTime += Time.unscaledDeltaTime;
+                if (currentTime > updateSpan)
+                {
+                    UpdateView();
+                    currentTime = 0f;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            playerStatus.UnresisterStatusUpdateEvent(UpdateView);
+            InputManager.Instance.inputActions.Debug.ShowDebugMenu.performed -= OnShowDebugMenu;
+        }
+
+        void UpdateView()
+        {
+            if (playerStatus != null)
+            {
+                coinText.text = $"コイン数:{playerStatus.coinNum}";
+                playTimeText.text = $"プレイ時間:{TimeManager.instance.GetPlayTimeString()}";
+            }
+        }
+
+        void OnShowDebugMenu(InputAction.CallbackContext context)
+        {
+            Debug.Log("デバックメニュー表示切り替え");
+            isShow = !isShow;
+            parentCanvasGroup.alpha = isShow ? 1 : 0;
+        }
+    }
+}
--- ProjectCronos/Assets/Scripts/Manager/DebugManager.cs
+++ ProjectCronos/Assets/Scripts/Manager/DebugManager.cs
@@ -9,8 +9,6 @@ namespace ProjectCronos
     /// </summary>
     class DebugManager : Singleton<DebugManager>
     {
-        bool isUseGraphy = true;
-
         GameObject graphy;
         string path = "Assets/Resources_moved/Prefabs/Graphy.prefab";
 
@@ -20,36 +18,19 @@ namespace ProjectCronos
         /// <returns>初期化に成功したかどうか</returns>
         public override async UniTask<bool> Initialize()
         {
-            if (isUseGraphy)
-            {
-                await AddressableManager.instance.LoadInstance(path,
-                    (obj) =>
-                    {
-                        graphy = obj;
-                    });
-            }
+            await AddressableManager.instance.LoadInstance(path,
+                (obj) =>
+                {
+                    graphy = obj;
+                });
 
             Debug.Log("DebugManager初期化");
-
-            InputManager.Instance.inputActions.Debug.ShowGraphy.performed += OnShowGraphy;
-
 
             return true;
         }
 
         void Update()
         {
-            if (InputManager.Instance.inputActions.Debug.ShowGraphy.WasPressedThisFrame())
-            {
-                Debug.Log("Graphy表示切り替え(Update)");
-                graphy.SetActive(!graphy.activeSelf);
-            }
-        }
-
-        void OnShowGraphy(InputAction.CallbackContext context)
-        {
-            Debug.Log("Graphy表示切り替え");
-            graphy.SetActive(!graphy.activeSelf);
         }
     }
 }
